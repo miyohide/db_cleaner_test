da9167f1a7cc6d5b86541296be94bd84b631018b
miyohide <miyohide@gmail.com>
2015-01-13 23:10:30 +0900

    add test.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,11 @@
 FactoryGirl.define do
   factory :user do
-    user_name "MyString"
-email_address "MyString"
+    user_name     "user1 name"
+    email_address "user1@example.com"
   end
 
+  factory :user2, class: User do
+    user_name     "user2 name"
+    email_address "user2@example.com"
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'test1' do
+    before do
+      @user = FactoryGirl.create(:user)
+    end
+
+    it 'username' do
+      expect(@user.user_name).to eq('user1 name')
+      expect(@user.email_address).to eq('user1@example.com')
+    end
+
+    it 'email' do
+      user = FactoryGirl.create(:user2)
+      expect(user.user_name).to eq('user2 name')
+      expect(user.email_address).to eq('user2@example.com')
+    end
+  end
+
 end
