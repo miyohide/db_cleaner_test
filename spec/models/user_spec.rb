5ab96eede0f241255560e3baafbbb38786d168c6
miyohide <miyohide@gmail.com>
2015-01-15 22:14:43 +0900

    case1. Not use database_cleaner pattern.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,20 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, :type => :model do
-  describe 'test1' do
+  describe 'sample test1' do
     before do
       @user = FactoryGirl.create(:user)
     end
 
-    it 'username' do
+    it 'username and email' do
       expect(@user.user_name).to eq('user1 name')
       expect(@user.email_address).to eq('user1@example.com')
-    end
-
-    it 'email' do
-      user = FactoryGirl.create(:user2)
-      expect(user.user_name).to eq('user2 name')
-      expect(user.email_address).to eq('user2@example.com')
     end
   end
 
