6778716d91b5457934fa5eca946b69eee6909064
miyohide <miyohide@gmail.com>
2015-01-19 21:43:49 +0900

    rails g scaffold post.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150112115002) do
+ActiveRecord::Schema.define(version: 20150119124237) do
+
+  create_table "posts", force: true do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "user_name"
