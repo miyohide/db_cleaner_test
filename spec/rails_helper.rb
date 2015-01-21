d209f5da5ae1a55e4a2e9f04532e63a631bd5cb9
miyohide <miyohide@gmail.com>
2015-01-22 00:04:42 +0900

    use database cleaner truncation strategy.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -34,7 +34,8 @@ RSpec.configure do |config|
   config.use_transactional_fixtures = false
 
   config.before(:suite) do
-    DatabaseCleaner.strategy = :transaction
+    # DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.strategy = :truncation
     DatabaseCleaner.clean_with(:truncation)
   end
 
